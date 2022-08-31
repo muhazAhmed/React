7b9e123ff21591ec06f405ce6abceda8c531a6ed
Muhaz <muhazvla313@gmail.com>
2022-08-31 17:43:41 +0530

    Styling with css

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import "./index.css"
 
 // To print current date and time.
 const currentDate = new Date().toLocaleDateString()
 const currentTime = new Date().toLocaleTimeString ()
+const image = "https://picsum.photos/200/300"
 
 ReactDom.render (
     <>
-    <div>
+    <div className='body'>
     <h1> Hello world! </h1>
     <p>Current Date is : {currentDate}</p>
     <p>Current Time is : {currentTime}</p>
+    <img src='{image}' alt='random Image' />
     </div>
     </>,
 
