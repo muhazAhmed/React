f9ac2e0d6905b7f665ce2aab033fa7f57a942eba
Muhaz <muhazvla313@gmail.com>
2022-09-08 12:10:10 +0530

    React-Component

--- src/index.js
+++ src/index.js
@@ -1,31 +1,18 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import "./index.css"
-
-// To print current date and time.
-let currentDate = new Date()
-currentDate = currentDate.getHours()
-let greeting = '';
-const style = {}
-
-if(currentDate >=0  && currentDate <= 11){
-    greeting = "Good Morning!";
-    style.color = "green"
-}else if(currentDate <= 16 && currentDate >= 12){
-    greeting ="Good Afternoon!";
-    style.color = "orchid"
-}else if(currentDate >= 16 && currentDate < 19){
-    greeting = "Good Evening!";
-    style.color = "blue"
-}else if(currentDate >= 19 && currentDate <= 24){
-    greeting = "Good Night";
-    style.color = "red"
-}
+import Heading from './Heading'
 
 ReactDom.render (
     <>
-    <div>
-    <h1>Hello, <span style={style}> {greeting} </span> </h1>
+    <div className='body'>
+    <Heading/>
+    <p>This is a paragraph</p>
+    <ol>
+        <li>Tag 1</li>
+        <li>Tage 2</li>
+        <li>Tag 3</li>
+    </ol>
     </div>
     </>,
 
