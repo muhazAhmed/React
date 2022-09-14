3130414430a7b5a258085b6f88e968c03dcaa820
Muhaz <muhazvla313@gmail.com>
2022-09-14 21:29:37 +0530

    Netflix card

---
+++ src/Sdata.jsx
@@ -0,0 +1,23 @@
+
+const Sdata = [
+    {
+        imgsrc : "https://wallpapercave.com/wp/wp4056410.jpg",
+        title : "A Netflix Original Series",
+        sname : "Dark",
+        link : "https://www.netflix.com/in/title/80100172"
+    },
+    {
+        imgsrc : "https://wallpapercave.com/dwp1x/wp10727665.jpg",
+        title : "A Netflix Original Series",
+        sname : "Peaky Blinders",
+        link : "https://www.netflix.com/in/title/80002479"
+    },
+    {
+        imgsrc : "https://wallpapercave.com/dwp1x/wp9920339.jpg",
+        title : "A Netflix Original Series",
+        sname : "Squid Game",
+        link : "https://www.netflix.com/in/title/81040344"
+    }
+];
+
+export default Sdata
--- src/index.js
+++ src/index.js
@@ -2,14 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Card from "./App";
 import './index.css'
+import Sdata from './Sdata'
 
 ReactDOM.render(
 <>
+<span className="netflix">
+        <img src="https://wallpapercave.com/dwp1x/wp5063339.png" alt="Netflix"/>
+        </span>
+        <h1 className="header">List of top Netflix Series</h1>
+        
+<span className="container">
 <Card
-        imgsrc = "https://wallpapercave.com/wp/wp4056410.jpg"
-        title = "A Netflix Original Series"
-        sname = "Dark"
-        link = "https://www.netflix.com/in/title/80100172"
+        imgsrc = {Sdata[0].imgsrc}
+        title = {Sdata[0].title}
+        sname = {Sdata[0].sname}
+        link = {Sdata[0].link}
 />
+<Card
+        imgsrc = {Sdata[1].imgsrc}
+        title = {Sdata[1].title}
+        sname = {Sdata[1].sname}
+        link = {Sdata[1].link}
+/>
+<Card
+        imgsrc = {Sdata[2].imgsrc}
+        title = {Sdata[2].title}
+        sname = {Sdata[2].sname}
+        link = {Sdata[2].link}
+/>
+
+</span>
 </>,
 document.getElementById('root'));
